48e5471ab9acdac820560ee3411012e452240c82
Amin Orai <amin.orai@gmail.com>
2024-03-09 10:25:36 +0330

    proj5

--- Crypto/src/App.jsx
+++ Crypto/src/App.jsx
@@ -4,12 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Crypto from './Components/Crypto'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- 
+        
         <Crypto></Crypto>
     </>
   )
--- Crypto/src/Components/Crypto.jsx
+++ Crypto/src/Components/Crypto.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { coinData } from "./coin-data";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid,GridToolbar } from "@mui/x-data-grid";
+import LinearProgress from '@mui/material/LinearProgress';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import { withTheme } from "@emotion/react";
+import { useDemoData } from '@mui/x-data-grid-generator';
+
 const Crypto = () => {
   const defaultCoins = useRef([]);
   const [coins, setCoins] = useState([]);
   const handleChange = (event) => {
     const newCoins = defaultCoins.current.filter(
       (coin) =>
-      coin.id.includes(event.target.value) ||
-      coin.symbol.includes(event.target.value)
+        coin.id.includes(event.target.value) ||
+        coin.symbol.includes(event.target.value)
     );
     setCoins(newCoins);
   };
+  
   const fetchData = async () => {
     try {
       const response = await fetch(
@@ -32,21 +38,41 @@ const Crypto = () => {
   };
   useEffect(() => {
     console.log("useEffect");
-
     fetchData();
+
+    const interval = setInterval(fetchData, 3000);
+
+    return () => clearInterval(interval);
   }, []);
-  const columns=[
-    { field: 'image', headerName: '', width: 40,renderCell: (params) => <img src={params.value} />},
-    { field: 'id', headerName: 'ID', width: 100  },
-  { field: 'name', headerName: 'name', width: 100 },
-  { field: 'current_price', headerName: 'price', width: 100 },
-  { field: 'high_24h', headerName: 'high_24h', width: 100 },
+ 
+  const columns = [
+    { field: "market_cap_rank", headerName: "Rank", width: 10 },
+    {
+      field: "image",
+      headerName: "Symbol Image",
+      width: 60,
+      renderCell: (params) => <img src={params.value} />,
+    },
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "name", headerName: "Name", width: 100 },
+    { field: "current_price", headerName: "Price", width: 100 },
+    { field: "high_24h", headerName: "High_24h", width: 100 },
+    { field: "low_24h", headerName: "Low_24h", width: 100 },
+    { field: "price_change_24h", headerName: "price-change-24", width: 100 },
+    { field: "total_volume", headerName: "total_volume", width: 100 },
+    { field: "price_change_percentage_24h", headerName: "Percentage-24", width: 100 },
+   
+   
+  ];
   
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   
-  ]
 
   return (
-    <div >
+    <div className="ring ring-green-500 rounded-lg  text-white">
+      
       {/* <Box
         component="form"
         sx={{
@@ -63,29 +89,39 @@ const Crypto = () => {
           label="Outlined"
           variant="outlined"
         ></TextField> */}
-        {/* <table  className="border w-full border-red-400" >{coins.map((coin) => (
+      {/* <table  className="border w-full border-red-400" >{coins.map((coin) => (
           <span key={coin.id}>
             <div>{coin.name}</div>
             <div>{coin.symbol}</div>
             <div>{coin.current_price}</div>
           </span>
         ))}</table> */}
-        
+
       {/* </Box> */}
-      <div style={{ height: 600, width: '100%' }}>
-      <DataGrid
-      className="bg-white opacity-60"
-        rows={coins}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 9 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
+      <div className="img-header  rounded-lg 	"><h1 className=" text-3xl font-mono font-bold tracking-widest ">Coin Market Cap</h1></div>
+      <div style={{ height: 580, width: "100%" }} className="bg-form  opacity-75 font-bold rounded-lg ">
+        <DataGrid
+         slots={{
+          loadingOverlay: LinearProgress,
+          toolbar: GridToolbar,
+          
+
+        }} 
+        sx={{fontFamily:'sans-serif',color:'black'}}  
+            
+        loading
+        {...coins}
+          rows={coins}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 8 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}  
+          checkboxSelection    
+        />      
+      </div>      
     </div>
   );
 };
